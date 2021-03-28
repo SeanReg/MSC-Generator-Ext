130afdad706af28029c081b42a6777556edbcbbf
Sean Regan <regan.sean.p@gmail.com>
2021-03-27 23:44:19 -0400

    Add support for saving to a file

--- src/extension.ts
+++ src/extension.ts
@@ -6,41 +6,37 @@ const spawnSync = require('child_process').spawnSync;
 import * as fs from 'fs';
 const path = require('path');
 
+let webviewPanel: vscode.WebviewPanel | undefined = undefined
+
+enum BrowserMessage {
+	SAVE_FILE
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let webviewPanel: vscode.WebviewPanel | undefined = undefined
-fs
+	
+
+	vscode.workspace.onDidSaveTextDocument(event => {
+		if (event.fileName.endsWith(".signalling") || event.fileName.endsWith(".signaling")) {
+			displayMSCFromFile(context, event.fileName)
+		}
+	})
+
 	context.subscriptions.push(
-		vscode.commands.registerCommand('msc-gen-editor.start', () => {
-			webviewPanel = webviewPanel ?? vscode.window.createWebviewPanel(
-				'msc-gen', // Identifies the type of the webview. Used internally
-				'MSC-Gen', // Title of the panel displayed to the user
-				vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-				{}
-			);
-
-			webviewPanel.webview.html = chartToHTML(context) ?? ""
-
-			webviewPanel.reveal()
-
-			webviewPanel.onDidDispose(() => {
-				webviewPanel = undefined
-			})
-		})
+		vscode.commands.registerCommand('msc-gen-editor.start', () => generateMsc(context))
 	);
 }
 
-function chartToHTML(context: vscode.ExtensionContext): string | undefined {
-	if (!fs.existsSync(context.globalStoragePath))
-		fs.mkdirSync(context.globalStoragePath)
-
+function generateMsc(context: vscode.ExtensionContext) {
+	checkGlobalStorage(context)
 
 	let inputFile = vscode.window.activeTextEditor?.document?.uri?.fsPath ?? ""
 	let fileExists = fs.existsSync(inputFile)
 
 	if (!fileExists) {
-		inputFile = path.join(context.globalStoragePath, "chart.signalling")
+		const tmpFile = "chart.signalling"
+		inputFile = path.join(context.globalStoragePath, tmpFile)
 		let curText = vscode.window.activeTextEditor?.document?.getText()
 		fs.writeFileSync(inputFile, curText)
 
@@ -51,13 +47,55 @@ function chartToHTML(context: vscode.ExtensionContext): string | undefined {
 	}
 
 	if (fileExists) {
-		const outputFile = path.join(context.globalStoragePath, "tmp.png")
-
-		let res = spawnSync("msc-gen",['-i', inputFile, '-o', outputFile, '-S', 'signalling'])
-		 return pngToWebpage(outputFile)
+		displayMSCFromFile(context, inputFile)
 	}
+}
 
-	return undefined
+function displayMSCFromFile(context: vscode.ExtensionContext, filePath: string) {
+	webviewPanel = webviewPanel ?? vscode.window.createWebviewPanel(
+		'msc-gen', // Identifies the type of the webview. Used internally
+		'MSC-Gen', // Title of the panel displayed to the user
+		vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+		{enableScripts: true}
+	);
+
+	webviewPanel.webview.html = chartToHTML(context, filePath) ?? ""
+	webviewPanel.webview.onDidReceiveMessage(message => {
+		switch (message.command as BrowserMessage) {
+			case BrowserMessage.SAVE_FILE:
+				savePNGFile(Buffer.from(message.file, 'base64'))
+				break;
+		}
+	})
+	webviewPanel.reveal()
+
+	webviewPanel.onDidDispose(() => {
+		webviewPanel = undefined
+	})
+}
+
+function savePNGFile(png: Buffer) {
+	vscode.window.showSaveDialog({filters: {'PNG Image': ['png']}}).then(filePath => {
+		if (filePath != null) {
+			fs.writeFileSync(filePath.fsPath, png)
+		}
+	})
+}
+
+function checkGlobalStorage(context: vscode.ExtensionContext) {
+	if (!fs.existsSync(context.globalStoragePath))
+		fs.mkdirSync(context.globalStoragePath)
+}
+
+function chartToHTML(context: vscode.ExtensionContext, inputFile: string): string | undefined {
+	checkGlobalStorage(context)
+
+	const outputFile = path.join(context.globalStoragePath, "tmp.png")
+	fs.unlinkSync(outputFile)
+
+	let res = spawnSync("msc-gen",['-i', inputFile, '-o', outputFile, '-S', 'signalling'])
+	return pngToWebpage(outputFile)
+
 }
 
 function pngToWebpage(png: string) {
@@ -72,7 +110,22 @@ function pngToWebpage(png: string) {
 		<title>Msc</title>
 	</head>
 	<body>
-		<img src="data:image/png;base64, ${buffer}" />
+		<button style="font-weight:bold;" onclick="saveClick()">Save As...</button>
+		<img style="margin-top:20px; display:block;" src="data:image/png;base64, ${buffer}" />
+
+		<script>
+			let vscode
+			(function() {
+				vscode = acquireVsCodeApi();
+			}())
+			function saveClick() {
+				vscode.postMessage({
+					command: ${BrowserMessage.SAVE_FILE},
+					file: "${buffer}"
+				})
+			}
+	    </script>
+		
 	</body>
 	</html>`;
 }
